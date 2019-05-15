8ebcafcfe2387a151d4d825571e5fc7a550996dc
Amar Teragundi <amarteragundi@gmail.com>
2019-05-15 20:53:24 +0530

    fixing extra async

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPost = async () => async dispatch => {
-  const response = await jsonPlaceholder.get("/post");
+export const fetchPost = () => async dispatch => {
+  const response = await jsonPlaceholder.get("/posts");
   dispatch({ type: "FETCH_POST", payload: response });
 };
