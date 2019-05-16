c183ebb4c454eab7b6ae7c5240eb98c1c23a4f96
Amar Teragundi <amarteragundi@gmail.com>
2019-05-16 12:08:08 +0530

    displaying user name

--- src/components/Users.js
+++ src/components/Users.js
@@ -4,15 +4,22 @@ import { fetchUser } from "../actions";
 
 class User extends React.Component {
   componentDidMount() {
-    this.props.fetchUser();
+    this.props.fetchUser(this.props.id);
   }
   render() {
-    return <div />;
+    const user = this.props.users.find(user => user.id === this.props.id);
+
+    if (!user) return null;
+    return <div className="header">{user.name}</div>;
   }
 }
 
+const mapStateToProps = state => {
+  return { users: state.users };
+};
+
 export default connect(
-  null,
+  mapStateToProps,
   {
     fetchUser
   }
